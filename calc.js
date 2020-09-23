d0f22c55cf516782636e69c2373aa3a25148559b
matt-blackbourn <matt.blackie114@gmail.com>
2020-09-24 09:04:40 +1200

    initial file structure, pseudocode

---
+++ calc.js
@@ -0,0 +1,8 @@
+//event listeners for clicks on all numbers and symbols
+//create temporary inputs array, and push numbers and symbols into array 
+//use temp string to build numbers, until symbol input is received
+//when symbol input recieved, turn string to number, and add to array
+//add symbol to array, using function connected to button
+//array should then contain number, symbol, number, symbol etc
+//when = sign inputted, loop through array, take number and symbol and +=, -= etc through whole array.
+//return final total, display this in display.
